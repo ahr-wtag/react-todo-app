3a557cc834135c735435ecc1fcf3716f8c12e6db
Md. Redwan Ahmed <redwan.ahmed@welldev.io>
2023-04-06 15:40:57 +0600

    added custom prop validation

--- src/components/TaskCard/index.jsx
+++ src/components/TaskCard/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import style from "components/TaskCard/index.module.scss";
+import { checkDateString } from "utils/helpers/propCustomValidation";
 const TaskCard = ({ task, createdTime }) => {
   return (
     <div className={style.container}>
@@ -11,6 +12,6 @@ const TaskCard = ({ task, createdTime }) => {
 };
 TaskCard.propTypes = {
   task: PropTypes.string.isRequired,
-  createdTime: PropTypes.string.isRequired,
+  createdTime: checkDateString,
 };
 export default TaskCard;
