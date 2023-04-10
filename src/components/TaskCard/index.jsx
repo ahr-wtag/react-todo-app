880b79b8ed1b40f2e4659fff904e3f6f8f0e3e99
Md. Redwan Ahmed <redwan.ahmed@welldev.io>
2023-04-10 10:56:39 +0600

    fixed date issue

--- src/components/TaskCard/index.jsx
+++ src/components/TaskCard/index.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import PropTypes from "prop-types";
 import style from "components/TaskCard/index.module.scss";
 import { checkDateString } from "utils/helpers/propCustomValidation";
+import { dateFormatter } from "utils/helpers/dateFormatter";
 const TaskCard = ({ task, createdTime }) => {
   return (
     <div className={style.container}>
       <p>{task}</p>
-      <p>{`Created at: ${createdTime}`}</p>
+      <p>{`Created at: ${dateFormatter(createdTime)}`}</p>
     </div>
   );
 };
---
+++ src/utils/helpers/dateFormatter.js
@@ -0,0 +1,3 @@
+export const dateFormatter = (date) => {
+  return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
+};
