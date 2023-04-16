fbab066eb4402db6c1f363670b69c5f704e5ca54
Md. Redwan Ahmed <redwan.ahmed@welldev.io>
2023-04-16 11:45:46 +0600

    (step-6) fixed marge

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -4,12 +4,12 @@ import { PAGINATION_LIMIT } from "utils/constant";
 import { useDispatch, useSelector } from "react-redux";
 import { paginationUpdate } from "store/actions/";
 import style from "components/Pagination/index.module.scss";
-const Pagination = ({ children, createButtonState, taskListLength }) => {
+const Pagination = ({ children, showCreateCard, taskListLength }) => {
   const pagination = useSelector((state) => state.paginationLength);
   const dispatch = useDispatch();
   const paginate = () => {
     if (pagination >= taskListLength) {
-      dispatch(paginationUpdate(PAGINATION_LIMIT - createButtonState));
+      dispatch(paginationUpdate(PAGINATION_LIMIT - showCreateCard));
     } else {
       dispatch(paginationUpdate(PAGINATION_LIMIT + pagination));
     }
@@ -26,7 +26,7 @@ const Pagination = ({ children, createButtonState, taskListLength }) => {
 Pagination.propTypes = {
   children: PropTypes.string.isRequired,
   taskListLength: PropTypes.number.isRequired,
-  createButtonState: PropTypes.bool.isRequired,
+  showCreateCard: PropTypes.bool.isRequired,
 };
 
 export default Pagination;
--- src/components/TaskList/index.jsx
+++ src/components/TaskList/index.jsx
@@ -3,30 +3,33 @@ import EditTaskCard from "components/TaskCard/EditTaskCard";
 import PropTypes from "prop-types";
 import React, { useState } from "react";
 
-const TaskList = ({ tasks }) => {
+const TaskList = ({ tasks, limit }) => {
   const [editableTask, setEditableTask] = useState(null);
-  return tasks.map((todo) =>
-    todo.id == editableTask ? (
-      <EditTaskCard
-        key={todo.id}
-        id={todo.id}
-        task={todo.task}
-        setEditableTask={setEditableTask}
-      />
-    ) : (
-      <TaskCard
-        key={todo.id}
-        id={todo.id}
-        task={todo.task}
-        createdTime={todo.createdTime}
-        completed={todo.completed}
-        setEditableTask={setEditableTask}
-      />
-    )
-  );
+  return tasks
+    .slice(0, limit)
+    .map((todo) =>
+      todo.id == editableTask ? (
+        <EditTaskCard
+          key={todo.id}
+          id={todo.id}
+          task={todo.task}
+          setEditableTask={setEditableTask}
+        />
+      ) : (
+        <TaskCard
+          key={todo.id}
+          id={todo.id}
+          task={todo.task}
+          createdTime={todo.createdTime}
+          completed={todo.completed}
+          setEditableTask={setEditableTask}
+        />
+      )
+    );
 };
 
 TaskList.propTypes = {
   tasks: PropTypes.array.isRequired,
+  limit: PropTypes.number.isRequired,
 };
 export default TaskList;
