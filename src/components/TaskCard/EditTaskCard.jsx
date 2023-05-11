8db9a3f501436f51b720ca0435b925ea70ed8617
Md. Redwan Ahmed <redwan.ahmed@welldev.io>
2023-05-11 15:13:08 +0600

    (step-5) fix css

--- src/components/TaskCard/EditTaskCard.jsx
+++ src/components/TaskCard/EditTaskCard.jsx
@@ -3,7 +3,7 @@ import { useDispatch } from "react-redux";
 import PropTypes from "prop-types";
 import { sanitizeText } from "utils/helpers/sanitizeText.js";
 import { editTask, completeTask } from "store/actions/";
-import style from "components/TaskCard/index.module.scss";
+import "components/TaskCard/index.scss";
 import { ICON_DELETE, DELETE_ICON_ALT_TEXT, KEY_ENTER } from "utils/constant";
 import { ICON_COMPLETE, COMPLETE_ICON_ALT_TEXT } from "utils/constant/images";
 
@@ -59,7 +59,7 @@ const EditTaskCard = ({ id, task, onEditableTasks }) => {
   }
 
   return (
-    <div className={style.container}>
+    <div className="task-card">
       <textarea
         name="task"
         id="task"
@@ -68,13 +68,13 @@ const EditTaskCard = ({ id, task, onEditableTasks }) => {
         autoFocus
         onFocus={sendCursorToEnd}
         onKeyDown={storeTaskOnEnter}
-        className={style.textarea}
+        className="task-card__textarea"
       ></textarea>
-      <small className={style.error}>{error && error}</small>
+      <small className="task-card__error">{error && error}</small>
 
-      <div className={style.action__button__container}>
+      <div className="task-card__action-button-container">
         <div>
-          <button className={style.button} onClick={onSave}>
+          <button className="task-card__button" onClick={onSave}>
             save
           </button>
         </div>
