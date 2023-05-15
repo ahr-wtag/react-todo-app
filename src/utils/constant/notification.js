6e83eaee99e5c5e808371ac3e6a0767b393bb27f
Md. Redwan Ahmed <redwan.ahmed@welldev.io>
2023-05-15 10:14:04 +0600

    (step-9) Change Toast Icon

--- src/utils/constant/notification.js
+++ src/utils/constant/notification.js
@@ -1,3 +1,5 @@
+import React from "react";
+import { ICON_TICK } from "utils/constant";
 export const NOTIFICATION_SETTINGS = {
   position: "top-center",
   autoClose: 5000,
@@ -9,10 +11,15 @@ export const NOTIFICATION_SETTINGS = {
   theme: "colored",
 };
 
+export const NOTIFICATION_SETTINGS_SUCCESS = {
+  ...NOTIFICATION_SETTINGS,
+  icon: <img src={ICON_TICK} alt="Success Icon" />,
+};
+
 export const NOTIFICATION_MESSAGE_ADD_TASK = "Task Created!";
 export const NOTIFICATION_MESSAGE_DELETE_TASK = "Task Deleted!";
 export const NOTIFICATION_MESSAGE_COMPLETE_TASK = "Task Completed!";
 export const NOTIFICATION_MESSAGE_UPDATE_TASK = "Task Updated!";
 export const NOTIFICATION_MESSAGE_EMPTY_TASK = "Task Title Can Not Be Empty!";
 export const NOTIFICATION_MESSAGE_PROCESSING_ERROR =
-  "We Couldn't Process Your Changes!";
+  "We Couldn't Save Your Changes!";
--- src/utils/notification/index.js
+++ src/utils/notification/index.js
@@ -1,10 +1,15 @@
 import { toast } from "react-toastify";
-import { NOTIFICATION_SETTINGS } from "utils/constant";
+import {
+  NOTIFICATION_SETTINGS,
+  NOTIFICATION_SETTINGS_SUCCESS,
+} from "utils/constant";
 
 export const showSuccessToast = (message) => {
-  toast.success(message, NOTIFICATION_SETTINGS);
+  toast.dismiss();
+  toast.success(message, NOTIFICATION_SETTINGS_SUCCESS);
 };
 
 export const showErrorToast = (message) => {
+  toast.dismiss();
   toast.error(message, NOTIFICATION_SETTINGS);
 };
