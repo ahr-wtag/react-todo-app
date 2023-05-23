a371ba3c734f27798c359dc8cfac9ddd27a8f20f
Md. Redwan Ahmed <redwan.ahmed@welldev.io>
2023-05-23 10:25:25 +0600

    (step-4) added complete attribute

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -3,6 +3,7 @@ import { ADD_TASK, DELETE_TASK, COMPLETE_TASK } from "store/constants";
 export const addTask = (task) => {
   task.id = Date.now().toString();
   task.createdDate = new Date();
+  task.completed = false;
 
   return {
     type: ADD_TASK,
