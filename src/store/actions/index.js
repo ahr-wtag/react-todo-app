dfffd0f9b47b288eb184a6db29e45dcfd0915cc8
Md. Redwan Ahmed <redwan.ahmed@welldev.io>
2023-05-23 10:23:31 +0600

    (step-2) remove unwanted attribute from action

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -3,7 +3,6 @@ import { ADD_TASK } from "store/constants";
 export const addTask = (task) => {
   task.id = Date.now().toString();
   task.createdDate = new Date();
-  task.completed = false;
 
   return {
     type: ADD_TASK,
